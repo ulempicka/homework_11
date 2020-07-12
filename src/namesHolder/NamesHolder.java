c7b60445146c1ecb4fa07c833cf041ca9d5f1fcc
ulempicka <urszula.lempicka@gmail.com>
2020-07-12 22:45:44 +0200

    Commit 2 - added new content to NamesHolder

--- src/namesHolder/NamesHolder.java
+++ src/namesHolder/NamesHolder.java
@@ -4,40 +4,32 @@ import java.util.Arrays;
 
 public class NamesHolder {
     private String[] names;
-//    int length;
 
-    public NamesHolder(String[] names) {        //jak dodac length w konstruktorze?
-        this.names = names;
+    public NamesHolder(int length) {
+        this.names = new String[length];
     }
-
-//    void add(String name){      //– dodaje imię do tablicy lub rzuca NullPointerException przy przekazainu wartości null,
-//        for (int i=0; i < names.length; i++) {
-//            try{
-//                names[i] = name;
-//            }catch (NullPointerException e){
-//                System.err.println("Imie nie moze byc null!");
-//            }
-//        }
-//    }
 
     void add(String name, int index) {
         if (name == null) {
             throw new NullPointerException("Imie nie moze byc null!");
         }else if(index >= size()){
-            throw new ArrayIndexOutOfBoundsException("W tablicy nie ma tyle miejsca!");
+            throw new ArrayIndexOutOfBoundsException("W tablicy nie ma tyle miejsca!");     //kontrolowany lub nie
         }
+//        else if (names[index].equalsIgnoreCase(name)){
+//            throw new IllegalArgumentException("Zduplikowane imie!");
+//        }
         else {
             names[index] = name;
         }
     }
 
     boolean contains(String name) {
-        for (int i = 0; i < names.length; i++) {
+        for (String s : names) {
+
             if (name == null) {
                 throw new IllegalArgumentException("Imie nie moze byc null!");
-            }
-            else {
-                return names[i].equals(name);
+            } else {
+                return s.equalsIgnoreCase(name);
             }
         }
         return false;
--- src/namesHolder/TestNamesHolder.java
+++ src/namesHolder/TestNamesHolder.java
@@ -2,19 +2,19 @@ package namesHolder;
 
 public class TestNamesHolder {
     public static void main(String[] args) {
-        String[] names = new String[3];
-        NamesHolder namesHolder = new NamesHolder(names);
+        //String[] names = new String[3];
+        NamesHolder namesHolder = new NamesHolder(3);
 
         try{
             //namesHolder.add(null,0);
-            namesHolder.add("Ania",0);
+            namesHolder.add("Karol",0);
             System.out.println(namesHolder.toString());
+            namesHolder.add("Ania",1);
             System.out.println("Czy tablica ma Anię? " + namesHolder.contains("Ania"));
-            namesHolder.add("Karol",1);
             namesHolder.add("Michał",2);
             System.out.println(namesHolder.toString());
             namesHolder.add("Kasia",3);
-        }catch (NullPointerException e){
+        } catch (NullPointerException e){
             System.err.println(e.getMessage());
         }catch (ArrayIndexOutOfBoundsException e){
             System.err.println(e.getMessage());
